cc10ec11adbfdb9a79a5ef0170e20a25ee74d340
dhdylan <dhdylan@gmail.com>
2023-08-21 17:40:27 -0400

    Working on spawning move arrows

--- Assets/SurfaceObject.cs
+++ Assets/SurfaceObject.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SurfaceObject : MonoBehaviour
 {
     [SerializeField]
-    MoveButton up;
-    [SerializeField]
-    MoveButton left;
-    [SerializeField]
-    MoveButton down;
-    [SerializeField]
-    MoveButton right;
+    BoxCollider boxCollider;
 
+    [SerializeField]
+    MoveButton[] moveButtons;
+
+    [SerializeField]
+    MoveButton moveButtonPrefab;
+
+    [SerializeField]
+    float moveButtonPadding = 0.5f;
+
+    [SerializeField]
     bool moveMode = false;
 
 
     private void Start()
     {
-        up.owningObject = this;
-        left.owningObject = this;
-        down.owningObject = this;
-        right.owningObject = this;
+        moveButtons = InstantiateMoveButtons();
     }
 
     public void ToggleMoveMode()
@@ -33,9 +35,26 @@ public class SurfaceObject : MonoBehaviour
 
     private void SetMoveButtonVisibility(bool show)
     {
-        up.gameObject.SetActive(show);
-        left.gameObject.SetActive(show);
-        down.gameObject.SetActive(show);
-        right.gameObject.SetActive(show);
+        
+    }
+
+    private MoveButton[] InstantiateMoveButtons()
+    {
+        MoveButton[] moveButtons = new MoveButton[4];
+
+        Vector3[] moveButtonOffsets = new Vector3[4];
+        moveButtonOffsets[0] = new Vector3(0f, 0f, boxCollider.bounds.extents.z + moveButtonPadding);
+        moveButtonOffsets[1] = new Vector3(-boxCollider.bounds.extents.x - moveButtonPadding, 0f, 0f);
+        moveButtonOffsets[2] = new Vector3(0f, 0f, -boxCollider.bounds.extents.z - moveButtonPadding);
+        moveButtonOffsets[3] = new Vector3(boxCollider.bounds.extents.x + moveButtonPadding, 0f, 0f);
+
+        for(int i=0; i<moveButtons.Length; i++)
+        {
+            moveButtons[i] = Instantiate(moveButtonPrefab, boxCollider.bounds.center + moveButtonOffsets[i], transform.rotation, transform);
+            moveButtons[i].transform.rotation = Quaternion.LookRotation(transform.TransformDirection(moveButtonOffsets[i]), Vector3.up);
+            moveButtons[i].transform.Rotate(new Vector3(0f, 90f, 0f));
+        }
+
+        return moveButtons;
     }
 }
